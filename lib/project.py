da215b3fb532da90aa749ef457364da35c64bbc8
Pradeep Suthar <sutharpradeep081@gmail.com>
2020-02-20 00:06:13 +0530

    update 3rd time

--- lib/project.py
+++ lib/project.py
@@ -8,7 +8,6 @@ class Project:
             project_files = ['views.py','urls.py','app.py']
             current_dir_path = os.getcwd()+"\\"+project_name   
             print("\n%s, your project is created"%(project_name))
-            list1 = ['views.py','urls.py','app.py']
             for i in range(len(project_files)):
                 abs_file_path = os.path.join(current_dir_path, project_files[i])
                 open(abs_file_path,"x")
